467b10d1f41a03eeddafe5d2259a632d025eb5e7
kaleemon <mohammed.kalimulla@gmail.com>
2024-08-14 16:08:01 +0530

    Create tests\src\Functional\DHLLocationFormTest.php

---
+++ tests\src\Functional\DHLLocationFormTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\dhl_location_finder\Tests\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the DHL Location Finder form.
+ *
+ * @group dhl_location_finder
+ */
+class DHLLocationFormTest extends BrowserTestBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static $modules = ['dhl_location_finder'];
+
+    /**
+     * Tests the DHL Location Finder form.
+     */
+    public function testForm() {
+        // Create a user with permission to access content.
+        $user = $this->drupalCreateUser(['access content']);
+        $this->drupalLogin($user);
+
+        // Visit the form page.
+        $this->drupalGet('/dhl-location-finder');
+        $this->assertSession()->statusCodeEquals(200);
+        $this->assertSession()->pageTextContains('Find Locations');
+
+        // Fill out and submit the form.
+        $this->submitForm([
+            'country_code' => 'DE',
+            'city' => 'Dresden',
+            'postal_code' => '01067',
+        ], 'Find Locations');
+
+        // Check that the response is displayed.
+        $this->assertSession()->pageTextContains('locationName');
+    }
+}
